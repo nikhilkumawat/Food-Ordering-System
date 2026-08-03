ba9102adbaeaa07e8352a659b2592135df2773cb
Nikhil <nikhilkumawatnk67@gmail.com>
2026-08-03 11:32:59 +0530

    First commit

---
+++ order-service/src/main/java/org/food/ordering/system/order/OrderServiceApplication.java
@@ -0,0 +1,14 @@
+package org.food.ordering.system.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
